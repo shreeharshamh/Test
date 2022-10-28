c567085bf15cb4990e481c62681244ebfc1e9525
Shree Harsha MH <shreeharshamh@hotmail.com>
2022-10-28 17:40:56 +0530

    Adding Collections features demo examples

---
+++ Java1.8Features/CollectionsConcept/HashSetDemo.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+        HashSet<Object> h = new HashSet<>();
+        h.add("Y");
+        h.add("A");
+        h.add("B");
+        h.add("X");
+        h.add("C");
+        h.add("Z");
+        h.add(null);
+        //duplicates are not allowed
+        System.out.println("Adding element Z to HashSet : " + h.add("Z"));
+        // ascending order arrangement
+        System.out.println(h);
+    }
+}
