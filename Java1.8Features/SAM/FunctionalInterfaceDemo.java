28ee7b641c92b89327104d6075a5a2c59f0a58a3
Shree Harsha MH <shreeharshamh@hotmail.com>
2022-10-31 17:15:44 +0530

    .

--- Java1.8Features/SAM/FunctionalInterfaceDemo.java
+++ Java1.8Features/SAM/FunctionalInterfaceDemo.java
@@ -1,15 +1,20 @@
 interface iface {
     public void add(int a, int b);
 }
-class demo implements iface {
+/*
+class Demo implements iface {
     public void add(int a, int b) {
         System.out.println(a+b);
     }
 }
-
+*/
 class FunctionalInterfaceDemo {
     public static void main(String[] args) {
         iface i = (a,b) -> System.out.println(a+b);
-        i.add(102, 302);
+        i.add(125, 301);
+        /* 
+        iface i = new Demo();
+        i.add(10, 20);
+        */
     }
 }
--- Singleton/Demo.java
+++ Singleton/Demo.java
@@ -1,5 +1,6 @@
-package Singleton;
+package Test.Singleton;
 // By declaring every every constructor as private we can restrict child class creation
+// For the below class A, it is impossible to create child class
 class A {
     // When class A is private, child class (B) cannot be created
     // that throws you compile time error
