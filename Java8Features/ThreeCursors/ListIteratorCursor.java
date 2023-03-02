71086cf39aaa1b485b8ac7cce712e972fd1e48a2
Shree Harsha MH <shreeharshamh@hotmail.com>
2023-03-02 09:41:53 +0530

    .
    .

--- Java8Features/ThreeCursors/ListIteratorCursor.java
+++ Java8Features/ThreeCursors/ListIteratorCursor.java
@@ -1,9 +1,26 @@
 package Test.Java8Features.ThreeCursors;
 
-import javax.sound.sampled.SourceDataLine;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class ListIteratorCursor {
     public static void main(String[] args) {
-        System.out.println("Hello wolrd");
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Nagendra");
+        list.add("Pinky");
+        list.add("Nishant");
+        list.add("Paramesh");
+
+        ListIterator<String> ltr = list.listIterator();
+        while(ltr.hasNext()) {
+            String S = (String) ltr.next();
+            if(S.equals("Pinky")) ltr.remove(); // [Nagendra, Nishant, Paramesh]
+            else if(S.equals("Nishant")) ltr.add("Santosh"); // [Nagendra, Nishant, Paramesh, Santosh]
+            else if(S.equals("Paramesh")) ltr.set("Janardhan"); // [Nagendra, Nishant, Janardhan, Santosh]
+            System.out.println(ltr.toString());
+        }
+        
+
     }    
+
 }
